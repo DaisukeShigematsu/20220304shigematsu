ebf3f21f42ec4aaad66d262c49618c809716c43b
DaisukeShigematsu <ex00.determination@gmail.com>
2022-03-26 01:41:38 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
-  const nav = document.getElementById("nav");
+  const nav = document.getElementById("drawer_nav");
   nav.classList.toggle('in');
 });
 
